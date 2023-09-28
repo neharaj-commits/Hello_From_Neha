4e0d88ad897671117f0485c354740c457c0b26e0
neharaj-commits <neha.rajagopalan@gmail.com>
2023-09-28 11:27:46 -0700

    Add script description + print additional line

--- pSet0.R
+++ pSet0.R
@@ -1,1 +1,8 @@
+#--------------------
+# AUTHOR: Neha Rajagopalan
+# PURPOSE: This is a test script to submit for Problem Set 1. Hoping to use R 
+#          to implement analysis methods and for data visualization in future 
+#          problem sets/research.
+#--------------------
 print("hello world!")
+print("This is the additional line after initial commit of the R script")
